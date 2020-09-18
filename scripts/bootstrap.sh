0a6708683001917ec35a8bbd98ca0104a3428097
Max Moon <moon.maxwell@gmail.com>
2020-09-17 21:52:00 -0700

    create table automatically

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ function create_cloudtrail() {
     TRAIL_BUCKET_NAME=$5
 
     echo "Creating trail.."
-    aws cloudtrail create-trail --name "${TRAIL_NAME}" --s3-bucket-name "${TRAIL_BUCKET_NAME}" "${TRAIL_REGIONAL_STATUS}" "${TRAIL_INCLUDE_GLOBAL_EVENTS}" "${TRAIL_PERFORM_LOG_VALIDATION}"
+    aws cloudtrail create-trail --name "${TRAIL_NAME}" --s3-bucket-name "${TRAIL_BUCKET_NAME}" "${TRAIL_REGIONAL_STATUS}" "${TRAIL_INCLUDE_GLOBAL_EVENTS}" "${TRAIL_PERFORM_LOG_VALIDATION}" > /dev/null 2>&1 && echo "Trail created successfully" || echo "Unable to create trail, please check your permissions and try again"
 }
 
 function prepare_and_create_cloudtrail() {
@@ -86,4 +86,56 @@ function prompt_to_create_cloudtrail() {
             *) echo "Please answer yes or no";;
         esac
     done
+}
+
+function create_athena_table() {
+    # https://docs.aws.amazon.com/ko_kr/cli/latest/reference/athena/start-query-execution.html
+    echo "creating athena table.."
+    QUERY_STRING=$1
+    TABLE_DATABASE=$2
+    TABLE_DATA_CATALOG=$3
+    QUERY_OUTPUT_LOC=$4
+
+    aws athena start-query-execution --query-string "${QUERY_STRING}" --query-execution-context Database="${TABLE_DATABASE}",Catalog="${TABLE_DATA_CATALOG}" --result-configuration OutputLocation="s3://${QUERY_OUTPUT_LOC}" > /dev/null 2>&1 && echo "Table created" || echo "Unable to execute Athena query, please try again"
+}
+
+function prepare_and_execute_sql_statement() {
+    TRAIL_BUCKET_NAME=$1
+    # Retrieve the account id which helps determine where the logs will be located
+    AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+
+    read -p "Enter the name of the database you'd like to create the table in (default: default): " TABLE_DATABASE
+    read -p "Enter the name of the data catalog the database is in (default: AwsDataCatalog): " TABLE_DATA_CATALOG
+
+    while true; do
+        read -p "Enter the name of your cloudtrail table: " TABLE_NAME
+
+        if [ ! -z "${TABLE_NAME}" ]; then
+            break
+        else
+            echo "Table name must be set"
+        fi
+    done
+
+    if [ -z "${TABLE_DATABASE}" ]; then
+        TABLE_DATABASE="default"
+    fi
+
+    if [ -z "${TABLE_DATA_CATALOG}" ]; then
+        TABLE_DATA_CATALOG="AwsDataCatalog"
+    fi
+
+    QUERY_STRING=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' -e "s/\${ATHENA_TABLE_NAME}/${TABLE_NAME}/" -e "s/\${ACCOUNT_ID}/${AWS_ACCOUNT_ID}/" -e "s/\${TRAIL_BUCKET_NAME}/${TRAIL_BUCKET_NAME}/" ./scripts/create_table.tpl.sql)
+
+    while true; do
+        read -p "Enter the name of the bucket and optionally a prefix for the output of the create table query (path optional): " QUERY_OUTPUT_LOC
+
+        if [ ! -z "${QUERY_OUTPUT_LOC}" ]; then
+            break
+        else
+            echo "Table name must be set"
+        fi
+    done
+
+    create_athena_table "${QUERY_STRING}" "${TABLE_DATABASE}" "${TABLE_DATA_CATALOG}" "${QUERY_OUTPUT_LOC}"
 }
