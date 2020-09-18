959b4f107457cbff1aec0b28c3a205e543187ecc
Max Moon <moon.maxwell@gmail.com>
2020-09-17 17:43:24 -0700

    improve logging format

--- reloader/main.py
+++ reloader/main.py
@@ -5,11 +5,11 @@ Automatically load newly created S3 prefixes into Athena.
 # Import from std lib
 from datetime import datetime, timedelta
 from functools import cached_property
+import json
 import logging
 import os
 from time import sleep
 from typing import TypedDict, Optional
-import sys
 
 # Import from
 import boto3
@@ -22,12 +22,33 @@ database = os.environ.get("DATABASE")
 table_name = os.environ.get("TABLE_NAME")
 output_loc = os.environ.get("OUTPUT_LOC")
 
-# Create the logger
+# Clear the default lambda logger so we can output json without INFO info and asctime
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger("athena-cloudtrail-reloader")
-logger.setLevel(logging.INFO)
-default_handler = logging.StreamHandler(sys.stderr)
-default_handler.setFormatter(logging.Formatter("%(message)s"))
-logger.addHandler(default_handler)
+
+
+class Encoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, set):
+            return tuple(o)
+        elif isinstance(o, str):
+            return o.encode("unicode_escape").decode("ascii")
+        elif isinstance(o, datetime):
+            return o.isoformat()
+        return super(Encoder, self).default(o)
+
+
+class JsonLog(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return Encoder().encode(self.kwargs)
 
 
 class ExecutionResponse(TypedDict):
@@ -67,6 +88,7 @@ class Athena:
             - str: from ["SUCCEEDED", "FAILED", "CANCELLED"]
         """
         status = "RUNNING"
+        resp = None
 
         while status not in ["SUCCEEDED", "FAILED", "CANCELLED"]:
             try:
@@ -77,7 +99,7 @@ class Athena:
                 status = resp["QueryExecution"]["Status"]["State"]
                 sleep(0.25)
 
-        return status
+        return resp
 
     def _process_execution_response(self, execution_response: dict) -> ExecutionResponse:
         """Takes the execution response from Athena's start_query_execution call
@@ -114,7 +136,15 @@ class Athena:
         else:
             return self._process_execution_response(execution_response=resp)
 
-    def succeeded(self, execution_response: ExecutionResponse, query: str) -> dict:
+    def succeeded(
+        self,
+        execution_response: ExecutionResponse,
+        query: str,
+        query_start: datetime,
+        query_end: datetime,
+        current_time: datetime,
+        x_amzn_request_id: str,
+    ) -> dict:
         """Handler for query succeeded.
 
         Args:
@@ -125,15 +155,30 @@ class Athena:
             - dictionary returned from self.results function
         """
         logger.info(
-            {
-                "response": f"Query '{query}' succeeded",
-                "status": "success",
-                "execution_id": execution_response["execution_id"],
-            }
+            JsonLog(
+                **{
+                    "response": "Query succeeded",
+                    "query": query,
+                    "status": "success",
+                    "execution_id": execution_response["execution_id"],
+                    "query_start": query_start,
+                    "query_end": query_end,
+                    "current_time": current_time,
+                    "x_amzn_request_id": x_amzn_request_id,
+                }
+            )
         )
         return self.results(execution_response)
 
-    def cancelled(self, execution_response: ExecutionResponse, query: str):
+    def cancelled(
+        self,
+        execution_response: ExecutionResponse,
+        query: str,
+        query_start: datetime,
+        query_end: datetime,
+        current_time: datetime,
+        x_amzn_request_id: str,
+    ):
         """Handler for query cancelled.
 
         Args:
@@ -144,18 +189,33 @@ class Athena:
             - Exception
         """
         logger.info(
-            {
-                "response": f"Query {query} cancelled",
-                "status": "failed",
-                "execution_id": execution_response["execution_id"],
-            }
+            JsonLog(
+                **{
+                    "response": "Query cancelled",
+                    "query": query,
+                    "status": "failed",
+                    "execution_id": execution_response["execution_id"],
+                    "query_start": query_start,
+                    "query_end": query_end,
+                    "current_time": current_time,
+                    "x_amzn_request_id": x_amzn_request_id,
+                }
+            )
         )
         # Set execution id because f strings don't like dict lookups
         execution_id = execution_response["execution_id"]
 
         raise Exception(f"Query cancelled. Query Execution ID: {execution_id}")
 
-    def failed(self, execution_response: ExecutionResponse, query: str):
+    def failed(
+        self,
+        execution_response: ExecutionResponse,
+        query: str,
+        query_start: datetime,
+        query_end: datetime,
+        current_time: datetime,
+        x_amzn_request_id: str,
+    ):
         """Handler for query failed.
 
         Args:
@@ -166,11 +226,18 @@ class Athena:
             - Exception
         """
         logger.info(
-            {
-                "response": f"Query {query} failed",
-                "status": "failed",
-                "execution_id": execution_response["execution_id"],
-            }
+            JsonLog(
+                **{
+                    "response": "Query failed",
+                    "query": query,
+                    "status": "failed",
+                    "execution_id": execution_response["execution_id"],
+                    "query_start": query_start,
+                    "query_end": query_end,
+                    "current_time": current_time,
+                    "x_amzn_request_id": x_amzn_request_id,
+                }
+            )
         )
         # Set execution id because f strings don't like dict lookups
         execution_id = execution_response["execution_id"]
@@ -187,11 +254,28 @@ class Athena:
             - Dict
         """
         execution_response = self.execute_query(query=query)
-        status = self.wait_for_completion(execution_response=execution_response)
+        resp = self.wait_for_completion(execution_response=execution_response)
+
+        # Retrieve necessary data from the resp
+        status = resp.get("QueryExecution", {}).get("Status", {}).get("State", None)
+        query_start = resp.get("QueryExecution", {}).get("Status", {}).get("SubmissionDateTime")
+        query_end = resp.get("QueryExecution", {}).get("Status", {}).get("CompletionDateTime")
+        x_amzn_request_id = resp.get("ResponseMetadata", {}).get("RequestId")
+
+        # Get current time for the log time
+        current_time = datetime.now()
 
         # Handle the status of the query dynamically
-        handler = getattr(self, status.lower(), None)
-        return handler(execution_response=execution_response, query=query)
+        if status:
+            handler = getattr(self, status.lower(), None)
+            return handler(
+                execution_response=execution_response,
+                query=query,
+                query_start=query_start,
+                query_end=query_end,
+                x_amzn_request_id=x_amzn_request_id,
+                current_time=current_time,
+            )
 
 
 class TablePartition:
@@ -287,7 +371,7 @@ class Event:
     @property
     def event_month(self) -> str:
         month = self.time.month
-        return str(month) if month >= 10 else str(month).zfill(2)
+        return str(month).zfill(2)
 
     @property
     def event_year(self) -> str:
@@ -296,7 +380,7 @@ class Event:
     @property
     def event_day(self) -> str:
         day = self.time.day
-        return str(day) if day >= 10 else str(day).zfill(2)
+        return str(day).zfill(2)
 
     def _convert_to_datetime(self, timestamp: str) -> Optional[datetime]:
         """Receives time in %Y-%m-%dT%H:%M:%S.%fZ format and returns float of seconds since epoch"""
@@ -331,7 +415,7 @@ class S3Helper:
         try:
             resp = self._client.get_bucket_lifecycle_configuration(Bucket=self._bucket)
         except ClientError as error:
-            logger.info(error.response)
+            logger.info(JsonLog(error=error.response))
             return None
         else:
             return self._parse_lifeycle_rules_for_expiration(resp.get("Rules", []))
@@ -353,7 +437,7 @@ class S3Helper:
                 Bucket=self._bucket, Prefix=f"AWSLogs/{self._account_id}/CloudTrail/", Delimiter="/"
             )
         except ClientError as error:
-            logger.info(error.response)
+            logger.info(JsonLog(error=error.response))
             return []
         else:
             return self._retrieve_regions(prefixes=resp["CommonPrefixes"])
@@ -371,7 +455,7 @@ class S3Helper:
                 try:
                     region = splitter[3]
                 except IndexError:
-                    logger.info("unable to find region")
+                    logger.info(JsonLog(error="unable to find region"))
                 else:
                     regions.append(region)
 
@@ -414,8 +498,8 @@ def lambda_handler(event, context) -> bool:
             old_partition = {
                 "region": region,
                 "year": str(drop_after_expire_date.year),
-                "month": str(drop_after_expire_date.month),
-                "day": str(drop_after_expire_date.day),
+                "month": str(drop_after_expire_date.month).zfill(2),
+                "day": str(drop_after_expire_date.day).zfill(2),
             }
 
             # Drop old partitions that don't contain data
